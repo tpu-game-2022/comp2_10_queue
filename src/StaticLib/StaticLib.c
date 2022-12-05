902836ad7b2b8ca32235380bc12363b213e4fdbb
bouningenEX <bouninngen@outlook.jp>
2022-12-05 13:51:49 +0900

    テストを通しました

--- src/StaticLib/StaticLib.c
+++ src/StaticLib/StaticLib.c
@@ -34,38 +34,85 @@ void finalize(QUEUE* q)
 // valの値をキューに入れる。実行の成否を返す
 bool enqueue(QUEUE* q, int val)
 {
+	if (q == NULL)
+		return false;
+	if (q->tail + 1 == q->memory_end)
+		return false;
+
+    *q->tail = val;
+	q->tail++;
+
+	    return true;
 	// ToDo: valのデータをキューに追加します
 	// 上手くいかない場合にはfalseを返します
 	// メモリを使い切ったら先頭アドレスに戻って追加して下さい
 
-	return false;
+	
 }
 
 
 // addrから始まるnum個の整数をキューに入れる。実行の成否を返す
 bool enqueue_array(QUEUE* q, int* addr, int num)
 {
+	if (q == NULL)
+		return false;
+	if (addr == NULL || num < 1 || num>countQueueableElements(q))
+		return false;
+
+	for (int i = 0; i < num; i++)
+	{
+		if (!enqueue(q, addr[i]))
+			return false;
+	}
 	// ToDo: addrからnum個のデータをキューに追加します
 	// 上手くいかない場合にはfalseを返します
 	// メモリを使い切ったら先頭アドレスに戻って追加して下さい
 
-	return false;
+	return true;
 }
 
 // キューから一つの要素を取り出す(不具合時は0を返す)
 int dequeue(QUEUE* q)
 {
+   if (q == NULL)
+		return 0;
+	if (isEmpty(q))
+		return 0;
 	// ToDo: 先頭のデータを返します
+	int*first = q->head++;
 
-	return 0;
+	if (q->head == q->tail)
+	{
+		q->head--;
+		q->tail--;
+	}
+
+	return *first;
 }
 
 // addrにキューからnumの要素を取り出す。取り出せた個数を返す
 int dequeue_array(QUEUE* q, int* addr, int num)
 {
 	// ToDo: 先頭からnum個のデータをaddrに格納します
+	if (q == NULL || addr == NULL || num<1)
+		return 0;
+	if (q->head == NULL)
+		return 0;
+	int q_count_element=0;
+	for(int i=0;i<num;i++)
+	{
+		if (countQueuedElements(q) <= 0)
+		{
+			break;
+		}
 
-	return 0;
+		addr[i] = dequeue(q);
+		q_count_element++;
+	}
+
+	return q_count_element++;
+
+	
 }
 
 // キューが空かどうかを調べる
